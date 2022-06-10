add0d86cbe772c59456c8d7c86562ee8ed69ae26
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-06-10 07:14:54 +0200

    Folders for components, data and pages set up. Initial draft generic list displayed.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React from "react";
+import { data } from "./data/data.js";
 
 function App() {
   return (
-    <div >
-      Press reader prototype
-    </div>
+    <>
+      <div>Press reader prototype</div>
+      <ul>
+      {data.map(d => <li key={d.id}>{d.title}</li> )}
+      </ul>
+</>
   );
 }
 
---
+++ src/data/data.js
@@ -0,0 +1,15 @@
+
+export const data = [
+    {
+        id: 1,
+        title: "First title"
+    },
+    {
+        id: 2,
+        title: "Second title"
+    },
+    {
+        id: 3,
+        title: "Third title"
+    },
+];
