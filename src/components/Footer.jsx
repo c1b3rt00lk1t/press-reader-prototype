2653f78ffd64a6cd4da633a107ec38b6fb98034a
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-06-10 08:31:42 +0200

    Added some sample button in the Footer and styled a bit.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
+import {IoMdSettings, IoMdEye, IoMdShareAlt, IoMdSearch} from 'react-icons/io'
+
 const Footer = () => {
   return (
-    <footer>Footer</footer>
+    <footer>
+        <IoMdSearch className='footer-icon'/>
+        <IoMdShareAlt className='footer-icon'/>
+        <IoMdEye className='footer-icon'/>
+        <IoMdSettings className='footer-icon'/>
+    </footer>
   )
 }
 
