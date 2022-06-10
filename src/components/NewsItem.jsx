90f37f21ff3e7e8e7cd6b630281db8dd6286d166
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-06-10 12:12:22 +0200

    Changed the gap space approach and uding margin (top, bottom) instead.

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -5,18 +5,18 @@ import { FaTag, FaIndustry, FaLocationArrow } from "react-icons/fa";
 const NewsItem = ({ item }) => {
   return (
     <li className="news-item">
-      <div className="vertical-gap">
-      <div className="horizontal align-items-center">
-            <FaTag className="news-item-tag" />
+      <div >
+      <div className="horizontal align-items-center margin-lines">
+            <FaTag className="news-item-tag " />
             <Tags tags={item.tags} />
           </div>
-        <div className="news-item-title line-clamp-2">{item.title}
+        <div className="news-item-title line-clamp-2 margin-lines">{item.title}
         </div>
-        <div className="news-item-source">
+        <div className="news-item-source margin-lines">
           {item.date} - {item.source}
         </div>
 
-        <div className="horizontal ">
+        <div className="horizontal margin-lines">
           <div className="horizontal  align-items-center vw-40">
             <FaLocationArrow className="news-item-tag" />
             <Tags  tags={item.zone} />
