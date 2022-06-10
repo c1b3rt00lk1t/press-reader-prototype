611cf897f77b015c21f7b963a7c6f93e307418cb
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-06-10 17:01:20 +0200

    Basic Router set up and fixed the launch.json issue (the port to 3000).

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import React from "react";
 import Footer from "./components/Footer.jsx";
 import NewsList from "./components/NewsList.jsx";
 import { data } from "./data/data.js";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <h1>Press reader</h1>
-      <NewsList data = {data}/>
-      <Footer />  
+      <Router>
+        <Routes>
+          <Route exact path="/"
+            element={
+              <>
+                <h1>Press reader</h1>
+                <NewsList data={data} />
+              </>
+            }
+          />
+          <Route path="/search" element={<h1>Search</h1>} />
+        </Routes>
+        <Footer />
+      </Router>
     </>
   );
 }
