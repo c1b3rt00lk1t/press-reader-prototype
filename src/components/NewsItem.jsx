b365dfb32219146ba92180ffba34a9c0b259047e
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-06-10 09:14:21 +0200

    Some more basic styling for the title, date and source of a NewsItem.

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
-const NewsItem = ({item}) => {
-  return <li className="news-item" >{item.title}</li>;
+const NewsItem = ({ item }) => {
+  return (
+    <li className="news-item">
+      <div>
+        <div className="news-item-title">{item.title}</div>
+        <div className="news-item-source">
+          {item.date} - {item.source}
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default NewsItem;
