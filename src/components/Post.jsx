2be495ecff9680c3119948079e9ee822a1899233
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-06-11 21:35:12 +0200

    Checking if onClick and onTouchStart can life together and increasing a bit the size of the next-previous buttons.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,35 +1,34 @@
-import React , {useContext} from "react";
+import React, { useContext } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { FaTag, FaIndustry, FaLocationArrow } from "react-icons/fa";
 import Tags from "./Tags";
 import PressReaderContext from "../contexts/PressReaderContext";
 
-import {MdNavigateBefore, MdNavigateNext} from "react-icons/md";
-
+import { MdNavigateBefore, MdNavigateNext } from "react-icons/md";
 
 const Post = ({ data }) => {
   const params = useParams();
   const item = data.filter((a) => `${a.id}` === params.id)[0];
   const indexItem = data.indexOf(item);
 
-  const {setSelectedPost} = useContext(PressReaderContext);
+  const { setSelectedPost } = useContext(PressReaderContext);
   const navigate = useNavigate();
 
   const handleTouch = (ev) => {
     let el = ev.target;
-    while(!el.classList.value.includes('on-touch')){
-        el = el.parentElement;
+    while (!el.classList.value.includes("on-touch")) {
+      el = el.parentElement;
     }
-    const sign = el.classList.value.includes('to-next') ? 1 : -1;
-    setSelectedPost(data[indexItem + (1 * sign) ].id);
-    navigate(`/post/${data[indexItem + ( 1 * sign) ].id}`);
-  }
+    const sign = el.classList.value.includes("to-next") ? 1 : -1;
+    setSelectedPost(data[indexItem + 1 * sign].id);
+    navigate(`/post/${data[indexItem + 1 * sign].id}`);
+  };
 
   return (
     <>
-      <div 
+      <div
         className="horizontal align-items-center margin-lines"
-        style={{ justifyContent: "space-between" , marginBottom: "2.5vh"}}
+        style={{ justifyContent: "space-between", marginBottom: "2.5vh" }}
       >
         <div className="horizontal align-items-center">
           <FaTag className="news-item-tag " />
@@ -44,14 +43,31 @@ const Post = ({ data }) => {
           <Tags tags={item.sector} />
         </div>
       </div>
-      
-      <div style={{position: "relative"}} >
-        <div className="on-touch to-previous horizontal align-items-center justify-items-center" onTouchStart={handleTouch}><MdNavigateBefore className="previous-next"/></div>
-        <div className="on-touch to-next horizontal align-items-center justify-items-center" onTouchStart={handleTouch}><MdNavigateNext  className="previous-next"/></div>
+
+      <div style={{ position: "relative" }}>
+        <div
+          className="on-touch to-previous horizontal align-items-center justify-items-center"
+          onTouchStart={handleTouch}
+          onClick={handleTouch}
+        >
+          <MdNavigateBefore className="previous-next" />
+        </div>
+        <div
+          className="on-touch to-next horizontal align-items-center justify-items-center"
+          onTouchStart={handleTouch}
+          onClick={handleTouch}
+        >
+          <MdNavigateNext className="previous-next" />
+        </div>
         {!!item.url && (
-          <embed 
+          <embed
             src={item.url}
-            style={{ margin: "-2.5vw", width: "100vw", height: "95vh", paddingBottom: "5vh" }}
+            style={{
+              margin: "-2.5vw",
+              width: "100vw",
+              height: "95vh",
+              paddingBottom: "5vh",
+            }}
             type="application/pdf"
           ></embed>
         )}
