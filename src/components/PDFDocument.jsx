c11689444a943edf323323f315bb71a853db06ca
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-06-13 22:44:23 +0200

    Test for styling PDF Document

--- src/components/PDFDocument.jsx
+++ src/components/PDFDocument.jsx
@@ -20,9 +20,17 @@ const PDFDocument = ({url}) => {
   }
   return (
     <div>
-      <Document file={url} onLoadSuccess={onDocumentLoadSuccess}>
-        <Page pageNumber={pageNumber} />
-      </Document>
+      
+
+      <Document
+      file={url}
+      options={{ workerSrc: "/pdf.worker.js" }}
+      onLoadSuccess={onDocumentLoadSuccess}
+    >
+      {Array.from(new Array(numPages), (el, index) => (
+        <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+      ))}
+    </Document>
       <p>
         Page {pageNumber} of {numPages}
       </p>
