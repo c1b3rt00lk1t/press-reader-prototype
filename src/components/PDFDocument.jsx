438ac3caca35c526bed10f5c2d4eba5d5bf1f5ec
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-06-13 23:18:11 +0200

    Removing warning from PDFDocument.

--- src/components/PDFDocument.jsx
+++ src/components/PDFDocument.jsx
@@ -14,7 +14,7 @@ const PDFDocument = ({url}) => {
 
   const [numPages, setNumPages] = useState(null);
 //   const [pageNumber, setPageNumber] = useState(1);
-  const [pageNumber] = useState(1);
+  // const [pageNumber] = useState(1);
   const onDocumentLoadSuccess = ({ numPages }) => {
     setNumPages(numPages);
   }
