09e0fe0f1ed8ec7c4f5c705362a9ada8e496875a
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-07-03 22:12:12 +0200

    Added Firebase and Real-time database basic functionality.

---
+++ src/firebase.js
@@ -0,0 +1,34 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, onValue } from "firebase/database";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDlDzlrEY-VkPZdNkEZOTSbrLJcL2LoQTo",
+  authDomain: "press-uploader.firebaseapp.com",
+  projectId: "press-uploader",
+  storageBucket: "press-uploader.appspot.com",
+  messagingSenderId: "496555378224",
+  databaseURL:
+    "https://press-uploader-default-rtdb.europe-west1.firebasedatabase.app/",
+  appId: "1:496555378224:web:323bd87185f34dfe5c8ff5",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+// Initialize Real-time database
+const database = getDatabase(app);
+
+export const getDataFromDB = (setNames) => {
+
+const refDB = ref(database, "/");
+onValue(refDB, (snapshot) => {
+  const data = snapshot.val();
+  console.log(data);
+  setNames(data.map(item => item.name))
+})
+};
