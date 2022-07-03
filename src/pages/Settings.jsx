e4446acddc574e6ebb1073ee9e5db5504da6f9c6
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-07-03 22:12:44 +0200

    Quick PoC to check if there are any compatibility issues.

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
+
+import {getDataFromDB} from "../firebase";
+
+
 
 const Settings = () => {
+  const [names,setNames] = useState([]);
+
   return (
     <>
       <h1>Settings</h1>
       <p style={{fontSize: '0.8em'}}>Prototype: 202206231251</p>
+      {names.map((name,i) => <p key={i}>{name}</p>)}
+      <button onClick={() => getDataFromDB(setNames)}>Get Data</button>
     </>
   );
 };
