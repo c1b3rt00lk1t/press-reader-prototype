4abdce065dd13034ff1e0dee78b32c49ea6f9a28
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-07-10 11:15:22 +0200

    First PWA approach PoC.

--- src/service-worker.js
+++ src/service-worker.js
@@ -11,7 +11,7 @@ import { clientsClaim } from 'workbox-core';
 import { ExpirationPlugin } from 'workbox-expiration';
 import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
-import { StaleWhileRevalidate } from 'workbox-strategies';
+import { CacheFirst } from 'workbox-strategies';
 
 clientsClaim();
 
@@ -46,12 +46,11 @@ registerRoute(
   createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
 );
 
-// An example runtime caching route for requests that aren't handled by the
-// precache, in this case same-origin .png requests like those from in public/
+// Runtime caching route 
 registerRoute(
-  // Add in any other file extensions or routing criteria as needed.
-  ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.pdf'), // Customize this strategy as needed, e.g., by changing to CacheFirst.
-  new StaleWhileRevalidate({
+  ({ url }) => url.origin === url.pathname.endsWith('.pdf'), 
+  // Customize this strategy as needed
+  new CacheFirst({
     cacheName: 'pdfs',
     plugins: [
       // Ensure that once this runtime cache reaches a maximum size the
