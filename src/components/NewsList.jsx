0fec7cb59058a2dbded9074fc70d1e9d2eeac3b2
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-07-12 17:50:43 +0200

    Added a warning and a limitation to display maximum 255 news.

--- src/components/NewsList.jsx
+++ src/components/NewsList.jsx
@@ -1,18 +1,20 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import PressReaderContext from "../contexts/PressReaderContext";
 import NewsItem from "./NewsItem";
 
 const NewsList = () => {
-
-  const {dataOrdered} = useContext(PressReaderContext)
+  const { dataOrdered } = useContext(PressReaderContext);
 
   return (
     <>
-      {!!dataOrdered.length && <ul>
-        {dataOrdered.map((item) => (
-          <NewsItem key={item.id} item={item} />
-        ))}
-      </ul>}
+      {dataOrdered.length > 255 && <div className="tags" style={{textAlign:"center"}}>{">"}255 results</div>}
+      {!!dataOrdered.length && (
+        <ul>
+          {dataOrdered.slice(0,255).map((item) => (
+            <NewsItem key={item.id} item={item} />
+          ))}
+        </ul>
+      )}
     </>
   );
 };
