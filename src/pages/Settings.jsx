74d597a7b0bd9886fb76010ea4af475d3ce3f212
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-07-13 16:02:43 +0200

    Updated version before push.

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -51,7 +51,7 @@ const Settings = () => {
     <div className="no-footer">
       <Offline />
       <h1>Settings</h1>
-      <p style={{ fontSize: "0.8em" }}>Prototype: v1.4.1</p>
+      <p style={{ fontSize: "0.8em" }}>Prototype: v1.4.2</p>
       <div
         className="vertical justify-items-space-between"
         style={{ height: "65vh" }}
