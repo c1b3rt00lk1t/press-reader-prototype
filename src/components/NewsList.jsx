a45625af4c8bcf04ec6896241e44bde8c0741364
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-08-25 14:07:42 +0200

    increased the max length of the news list

--- src/components/NewsList.jsx
+++ src/components/NewsList.jsx
@@ -4,13 +4,13 @@ import NewsItem from "./NewsItem";
 
 const NewsList = ({texts,language}) => {
   const { dataOrdered } = useContext(PressReaderContext);
-
+  const MAX_SHOW = 1500;
   return (
     <>
-      {dataOrdered.length > 255 && <div className="tags" style={{textAlign:"center"}}>{">"}255 {texts[language].main.results}</div>}
+      {dataOrdered.length > MAX_SHOW && <div className="tags" style={{textAlign:"center"}}>{">"}{MAX_SHOW} {texts[language].main.results}</div>}
       {!!dataOrdered.length && (
         <ul>
-          {dataOrdered.slice(0,255).map((item) => (
+          {dataOrdered.slice(0,MAX_SHOW).map((item) => (
             <NewsItem key={item.id} item={item} />
           ))}
         </ul>
