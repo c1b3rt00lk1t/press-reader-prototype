e487267cad725db02519678e85f7b542a655750a
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-07-06 16:50:55 +0200

    Adjusted comments.

--- src/indexedDB/indexedDB.js
+++ src/indexedDB/indexedDB.js
@@ -1,22 +1,23 @@
-import { openDB } from 'idb';
 // using https://github.com/jakearchibald/idb
+import { openDB } from 'idb';
 
 
 const dbname = 'PressReader';
 const version = 1;
 
-
-export const dbPromise = openDB(dbname, version, {
+// opens de database
+const dbPromise = openDB(dbname, version, {
     upgrade(db) {
       db.createObjectStore('PrReSessionList');
     },
   });
   
-
+// function to put/update and item in an object store (obj)
 export async function set(obj, key, val) {
     return (await dbPromise).put(obj, val, key);
 }
 
+// function to get the item with key (key) from an object store (obj)
 export async function get(obj,key) {
     return (await dbPromise).get(obj, key);
 }
