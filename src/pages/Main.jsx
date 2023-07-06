bbef2fd0d752f50f424df267b4e535c97a3375cb
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-07-06 16:54:10 +0200

    Removed helper component to display the sessionList.

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,17 +3,14 @@ import LanguageContext from "../contexts/LanguageContext";
 
 import NewsList from "../components/NewsList";
 import Offline from "../components/Offline";
-import PressReaderContext from "../contexts/PressReaderContext";
 
 const Main = () => {
   const {texts, language} = useContext(LanguageContext)
-  const {sessionListSorted} = useContext(PressReaderContext)
   return (
     <>
       <div className="no-footer">
         <Offline />
         <h1>{texts[language].main.title}</h1>
-        <p>{sessionListSorted.toString()}</p>
         <NewsList texts={texts} language={language}/>
       </div>
     </>
