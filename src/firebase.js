7097afb0f64cd4e94722d37f6d761d4fc0773a01
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-07-11 12:01:54 +0200

    Firebase source dependending on environment with logic encapsulated in a separate module.

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, onValue } from "firebase/database";
+import { firebaseConfig } from "./firebaseEnv.mjs";
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDlDzlrEY-VkPZdNkEZOTSbrLJcL2LoQTo",
-  authDomain: "press-uploader.firebaseapp.com",
-  projectId: "press-uploader",
-  storageBucket: "press-uploader.appspot.com",
-  messagingSenderId: "496555378224",
-  databaseURL:
-    "https://press-uploader-default-rtdb.europe-west1.firebasedatabase.app/",
-  appId: "1:496555378224:web:323bd87185f34dfe5c8ff5",
-};
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
---
+++ src/firebaseEnv.mjs
@@ -0,0 +1,25 @@
+const firebaseConfig_DEV = {
+    apiKey: "AIzaSyDUy1hFE-VOkWqOS0FmeNakeqp531kVdx0",
+    authDomain: "press-uploader-2.firebaseapp.com",
+    databaseURL: "https://press-uploader-2-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "press-uploader-2",
+    storageBucket: "press-uploader-2.appspot.com",
+    messagingSenderId: "75363353539",
+    appId: "1:75363353539:web:11077c15544e85eed47376"
+  
+  };
+
+const firebaseConfig_PRD = {
+    apiKey: "AIzaSyDlDzlrEY-VkPZdNkEZOTSbrLJcL2LoQTo",
+    authDomain: "press-uploader.firebaseapp.com",
+    projectId: "press-uploader",
+    storageBucket: "press-uploader.appspot.com",
+    messagingSenderId: "496555378224",
+    databaseURL:
+      "https://press-uploader-default-rtdb.europe-west1.firebasedatabase.app/",
+    appId: "1:496555378224:web:323bd87185f34dfe5c8ff5",
+  };
+
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+export const firebaseConfig = isDevelopment ? firebaseConfig_DEV : firebaseConfig_PRD;
