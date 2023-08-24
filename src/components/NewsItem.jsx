78862344b1504624e6e448d2512145f8c218e276
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-08-24 16:18:08 +0200

    Delete console log

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -39,9 +39,7 @@ const NewsItem = ({ item }) => {
   const writeTextInClipboard = (text) => {
     navigator.clipboard
       .writeText(text)
-      .then(() => {
-        // console.log("Text copied to clipboard");
-      })
+      .then(() => {})
       .catch((err) => {
         console.error("Failed to copy text: ", err);
       });
