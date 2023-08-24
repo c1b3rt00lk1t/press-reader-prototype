f90cf7484974555f58593a6ac2affacab4f96f6f
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-08-24 21:11:08 +0200

    Hyperlink to clipboard

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -45,15 +45,51 @@ const NewsItem = ({ item }) => {
       });
   };
 
+  const writeInClipBoard = (title, date, source, url2) => {
+    const content = `<a href="${url2}" target="_blank" rel="noopener noreferrer">${title} (${source}, ${date.slice(
+      0,
+      4
+    )}-${date.slice(5, 6).padStart(2, "0")}-${date
+      .slice(7, 8)
+      .padStart(2, "0")})</a>`;
+
+    try {
+      // const blobInputText = new Blob([content], { type: "text/plain" });
+      const blobInput = new Blob([content], { type: "text/html" });
+      const clipboardItemInput = new window.ClipboardItem({
+        // "text/plain": blobInputText,
+        "text/html": blobInput,
+      });
+      navigator.clipboard.write([clipboardItemInput]);
+    } catch (e) {
+      // Handle error with user feedback - "Copy failed!" kind of thing
+      console.log(e);
+    }
+  };
+
+  // const readFromClipboard = async () => {
+  //   try {
+  //     const clipboardItems = await navigator.clipboard.read();
+
+  //     for (const clipboardItem of clipboardItems) {
+  //       console.log(clipboardItem);
+  //       for (const type of clipboardItem.types) {
+  //         const blob = await clipboardItem.getType(type);
+  //         console.log(blob);
+  //         console.log(type, await blob.text());
+  //       }
+  //     }
+  //   } catch (err) {
+  //     console.error(err.name, err.message);
+  //   }
+  // };
+
   const handleTitleClick =
-    ({ title, date, source }) =>
+    ({ title, date, source, url2 }) =>
     (ev) => {
       ev.stopPropagation();
-      writeTextInClipboard(
-        `${title} (${source}, ${date.slice(0, 4)}-${date
-          .slice(5, 6)
-          .padStart(2, "0")}-${date.slice(7, 8).padStart(2, "0")})`
-      );
+      writeInClipBoard(title, date, source, url2);
+      // readFromClipboard();
     };
 
   const handleLinkClick =
