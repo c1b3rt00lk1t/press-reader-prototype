aa15390c7be027d105fd1e4677342bf9b1efad9e
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-12-14 01:00:47 +0100

    Test it changes pages

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,13 @@
 describe("template spec", () => {
-  it("passes", () => {
+  beforeEach(() => {
     cy.viewport(600, 900);
     cy.visit("http://localhost:3000/");
+  });
+
+  it("changes pages", () => {
+    cy.get("[aria-label='footer-search']").click();
+    cy.get("[aria-label='footer-list']").click();
+    cy.get("[aria-label='footer-settings']").click();
+    cy.get("[aria-label='footer-share']").click();
   });
 });
