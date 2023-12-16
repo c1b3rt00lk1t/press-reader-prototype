0a68a34eae766c847cc6074b936efe70866b6550
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-12-17 00:43:33 +0100

    Settings component basic set up

--- src/pages/Settings.cy.jsx
+++ src/pages/Settings.cy.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 import Settings from "./Settings";
-import { withPressReaderContext } from "../helpers/withPressReaderContext";
-import { withLanguageContext } from "../helpers/withLanguageContext";
-import { pipe } from "../helpers/pipe";
+import { withAppContext } from "../helpers/withAppContext";
 
 describe("<Settings />", () => {
   it("renders", () => {
-    // see: https://on.cypress.io/mounting-react
-    // cy.mount(withLanguageContext(withPressReaderContext(<Settings />)));
-    cy.mount(pipe(withPressReaderContext, withLanguageContext)(<Settings />));
+    cy.mount(withAppContext(<Settings />));
   });
 });
