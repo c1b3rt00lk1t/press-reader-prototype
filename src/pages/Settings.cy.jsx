b45c51a7482510445ee3e42ac26110956d171495
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-12-17 09:50:47 +0100

    Test is changes language

--- src/pages/Settings.cy.jsx
+++ src/pages/Settings.cy.jsx
@@ -7,4 +7,14 @@ describe("<Settings />", () => {
     cy.mount(withAppContext(<Settings />));
     cy.contains("Configuración");
   });
+
+  it("changes language", () => {
+    cy.mount(withAppContext(<Settings />));
+    cy.get("[aria-label=settings-English]").click();
+    cy.contains("Settings");
+    cy.get("[aria-label=settings-Castellano]").click();
+    cy.contains("Configuración");
+    cy.get("[aria-label=settings-Italiano").click();
+    cy.contains("Configurazione");
+  });
 });
