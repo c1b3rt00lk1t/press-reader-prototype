800ebfe7f07621a21fd7688f1b6f350e4ae65003
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-12-17 09:28:37 +0100

    Add css styles to with App context for testing

--- src/helpers/withAppContext.jsx
+++ src/helpers/withAppContext.jsx
@@ -2,6 +2,7 @@ import { pipe } from "./pipe";
 import { withClipboardContext } from "./withClipboardContext";
 import { withLanguageContext } from "./withLanguageContext";
 import { withPressReaderContext } from "./withPressReaderContext";
+import "../index.css";
 
 export const withAppContext = (component) => {
   return pipe(
