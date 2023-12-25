48847735bbad1d539c42afdcf25c7f7a2b580801
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-12-25 11:14:28 +0100

    Set up node events with coverage task

--- cypress.config.mjs
+++ cypress.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from "cypress";
+import coverageTask from "@cypress/code-coverage/task.js";
 
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
+    setupNodeEvents(on, config) {
+      coverageTask(on, config);
+      return config;
+    },
   },
 
   component: {
