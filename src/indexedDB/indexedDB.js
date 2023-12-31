6e975e561be4af4c4ee430d8008d9262ffe6aa26
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-12-31 20:01:51 +0100

    Add istanbul ignore db upgrade

--- src/indexedDB/indexedDB.js
+++ src/indexedDB/indexedDB.js
@@ -1,24 +1,23 @@
 // using https://github.com/jakearchibald/idb
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
-
-const dbname = 'PressReader';
+const dbname = "PressReader";
 const version = 1;
 
 // opens de database
 const dbPromise = openDB(dbname, version, {
-    upgrade(db) {
-      db.createObjectStore('PrReSessionList');
-    },
-  });
-  
+  /*istanbul ignore next*/
+  upgrade(db) {
+    db.createObjectStore("PrReSessionList");
+  },
+});
+
 // function to put/update and item in an object store (obj)
 export async function set(obj, key, val) {
-    return (await dbPromise).put(obj, val, key);
+  return (await dbPromise).put(obj, val, key);
 }
 
 // function to get the item with key (key) from an object store (obj)
-export async function get(obj,key) {
-    return (await dbPromise).get(obj, key);
+export async function get(obj, key) {
+  return (await dbPromise).get(obj, key);
 }
-
